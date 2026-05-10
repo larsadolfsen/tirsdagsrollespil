c3838ae4b62658809bbecd19b5ad137174e24035
larsadolfsen <larsadolfsen@gmail.com>
2026-05-10 20:28:45 +0200

    Register new characters

--- src/data/characters/index.ts
+++ src/data/characters/index.ts
@@ -1,7 +1,9 @@
 import type { CharacterRecord } from "../../types";
+import { gerlardo } from "./gerlardo";
+import { tag } from "./tag";
 import { thanoVoss } from "./thano-voss";
 
-export const characterRecords: CharacterRecord[] = [thanoVoss];
+export const characterRecords: CharacterRecord[] = [thanoVoss, tag, gerlardo];
 
 export const characterRecordById = Object.fromEntries(
   characterRecords.map((character) => [character.id, character]),
