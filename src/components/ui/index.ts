5e22bd2fdbf6a943e868009044d4f8c9229ff4c7
larsadolfsen <larsadolfsen@gmail.com>
2026-05-11 09:33:18 +0200

    Add shared UI helper exports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,5 @@
+export { AdvancementSection } from "./AdvancementSection";
+export { HeaderResourceSlider, ResourceCounterBar } from "./ResourceCounterBar";
+export { InlineSubtabs } from "./InlineSubtabs";
+export { PanelSectionHeader } from "./PanelSectionHeader";
+export { ScrollableTabStrip } from "./ScrollableTabStrip";
