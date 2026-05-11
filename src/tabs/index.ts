17206b3e784e5e2d92bf2398a144764cbce768b0
larsadolfsen <larsadolfsen@gmail.com>
2026-05-11 09:38:23 +0200

    Export shared tab options and types

--- src/tabs/index.ts
+++ src/tabs/index.ts
@@ -7,3 +7,15 @@ export { SkillsTab } from "./SkillsTab";
 export { SpellsTab } from "./SpellsTab";
 export { TalentsTab } from "./TalentsTab";
 export { TabLoadingFallback } from "./TabLoadingFallback";
+export { mainTabOptions, mobilePageTitleByView, mobileTabMenuOptions } from "./tabOptions";
+export type {
+  ActionCategory,
+  CareerSubtab,
+  CoinKey,
+  InventorySubtab,
+  MainTab,
+  MobileTabMenuTarget,
+  SkillSubtab,
+  SpellSubtab,
+  TabOption,
+} from "./tabTypes";
