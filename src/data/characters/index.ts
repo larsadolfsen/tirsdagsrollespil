62865c1893b61434e32771e5dc8504ffbb783f71
larsadolfsen <larsadolfsen@gmail.com>
2026-05-11 17:54:11 +0200

    Remove duplicate character records

--- src/data/characters/index.ts
+++ src/data/characters/index.ts
@@ -1,9 +1,9 @@
 import type { CharacterRecord } from "../../types";
 import { karlMuller } from "./karl-muller";
-import { gerlardo } from "./gerlardo";
+import { gerhardLehrmann } from "./gerlardo";
 import { thanoVoss } from "./thano-voss";
 
-export const characterRecords: CharacterRecord[] = [thanoVoss, karlMuller, gerlardo];
+export const characterRecords: CharacterRecord[] = [thanoVoss, karlMuller, gerhardLehrmann];
 
 export const characterRecordById = Object.fromEntries(
   characterRecords.map((character) => [character.id, character]),
