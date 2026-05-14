f4a6fb77ac8955b329c076b8b3ca48619b2c47e7
larsadolfsen <larsadolfsen@gmail.com>
2026-05-14 09:50:08 +0200

    Export shared textarea primitive

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -11,7 +11,7 @@ export type { ButtonProps } from "./button";
 export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card";
 export { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogOverlay, DialogPortal, DialogTitle, DialogTrigger } from "./dialog";
 export { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuPortal, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from "./dropdown-menu";
-export { Input } from "./input";
+export { Input, Textarea } from "./input";
 export { Label } from "./label";
 export { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectSeparator, SelectTrigger, SelectValue } from "./select";
 export { Separator } from "./separator";
