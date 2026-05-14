31001c6ac11c720353fe1b4433341e09a5ddd6cb
larsadolfsen <larsadolfsen@gmail.com>
2026-05-14 09:32:21 +0200

    Export WFRP sheet data table primitives

--- src/components/wfrp/index.ts
+++ src/components/wfrp/index.ts
@@ -1,3 +1,11 @@
 export { WfrpPanel } from "./WfrpPanel";
 export { WfrpSection } from "./WfrpSection";
 export { WfrpStatusBadge } from "./WfrpStatusBadge";
+export {
+  SheetDataButtonRow,
+  SheetDataHeader,
+  SheetDataPanel,
+  SheetDataRow,
+  SheetDataTable,
+  SheetEmptyState,
+} from "./SheetDataTable";
