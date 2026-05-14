5077a9607a3305014bb326c1fefbf7896cd99fed
larsadolfsen <larsadolfsen@gmail.com>
2026-05-14 15:13:37 +0200

    Show corruption test title in dice roller

--- src/hooks/useDiceRoller.ts
+++ src/hooks/useDiceRoller.ts
@@ -152,6 +152,7 @@ export function useDiceRoller({
   const getTestTypeTitle = (testType: RollState["testType"] | RollHistoryItem["testType"]) => {
     if (testType === "attack") return "Attack Test";
     if (testType === "channeling") return "Channeling Test";
+    if (testType === "corruption") return "Corruption Test";
     return "Dramatic Test";
   };
 
