ce25025d746b588919c5ce63b9aa794917365df8
larsadolfsen <larsadolfsen@gmail.com>
2026-05-14 15:39:43 +0200

    Export shared sheet data list components

--- src/components/wfrp/index.ts
+++ src/components/wfrp/index.ts
@@ -6,6 +6,13 @@ export {
   SheetDataTable,
   SheetEmptyState,
 } from "./SheetDataTable";
+export {
+  SheetDataList,
+  SheetDataListRow,
+  SheetDataMobileDetails,
+  SheetRowActionButton,
+} from "./SheetDataList";
+export type { SheetDataMobileDetailField } from "./SheetDataList";
 export { WfrpPanel } from "./WfrpPanel";
 export { WfrpSection } from "./WfrpSection";
 export { WfrpStatusBadge } from "./WfrpStatusBadge";
