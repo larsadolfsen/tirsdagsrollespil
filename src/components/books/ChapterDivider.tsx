d2a6e7071f035dea72d6e17ef93222d5692abf64
larsadolfsen <larsadolfsen@gmail.com>
2026-07-02 17:35:11 +0200

    Update ChapterDivider.tsx

--- src/components/books/ChapterDivider.tsx
+++ src/components/books/ChapterDivider.tsx
@@ -3,7 +3,7 @@ import { Heading } from "../ui";
 
 export function ChapterHeading({ id, children }: { id?: string; children: ReactNode }) {
   return (
-    <div className="mb-3 inline-block max-w-full border-b border-wfrp-red">
+    <div className="mb-3 inline-block max-w-full border-b-2 border-wfrp-red">
       <Heading id={id} level={1} variant="chapterH1">{children}</Heading>
     </div>
   );
